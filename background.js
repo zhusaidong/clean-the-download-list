7b30fb6f4b9d17cb568fff69c2dd628dc669e69e
zhusaidong <635925926@qq.com>
2024-07-02 15:13:27 +0800

    feature: 升级manifest_version到v3

--- background.js
+++ background.js
@@ -1,16 +1,17 @@
 //@document https://developer.chrome.com/extensions/downloads#method-erase
 chrome.downloads.onChanged.addListener(function(downloadDelta) {
 	//download status = complete or user canceled it
-    if(downloadDelta.state != undefined && (downloadDelta.state.current == "complete" || (downloadDelta.state.current == "interrupted" && downloadDelta.error.current == "USER_CANCELED"))){
+    if(downloadDelta.state !== undefined && (downloadDelta.state.current === "complete" || (downloadDelta.state.current === "interrupted" && downloadDelta.error.current === "USER_CANCELED"))){
         setTimeout(function(){
-            chrome.downloads.erase({id:downloadDelta.id});
+			console.log("erase the downloads");
+            console.log("downloadDelta", downloadDelta)
+            chrome.downloads.erase({id:downloadDelta.id}).then(ids=>{
+                console.log("erase the ids", ids);
+            });
 			//Download reminder bar at the bottom of the page
-			setTimeout(function(){
-				chrome.downloads.setShelfEnabled(false);
-				setTimeout(function(){
-					chrome.downloads.setShelfEnabled(true);
-				},500);
-			},500);
+			chrome.downloads.setShelfEnabled(false);
+            chrome.downloads.setShelfEnabled(true);
+            //chrome.downloads.setUiOptions({enabled:false})
         },5000);
     }
 });
